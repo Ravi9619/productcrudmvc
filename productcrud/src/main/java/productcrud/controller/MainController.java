301954a7a1ce8554a39df1a014e1adf7a946b6e0
Ravi <dubeyravi9619@gmail.com>
2023-03-02 14:46:03 +0530

    CRUD App using spring mvc completed

--- productcrud/src/main/java/productcrud/controller/MainController.java
+++ productcrud/src/main/java/productcrud/controller/MainController.java
@@ -1,20 +1,33 @@
 package productcrud.controller;
 
-import org.apache.taglibs.standard.lang.jstl.test.beans.PublicBean1;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.view.RedirectView;
 
+import productcrud.dao.ProductDao;
 import productcrud.model.Product;
 
 @Controller
 public class MainController {
 	
+	@Autowired
+	private ProductDao productDao;
+	
 	@RequestMapping("/")
-	public String home() {
+	public String home(Model model) {
+		
+		List<Product> products = productDao.getProducts();
+		model.addAttribute("products", products);
+		
 		return "index";
 	}
 	
@@ -26,9 +39,30 @@ public class MainController {
 	
 	//handle add product form
 	@RequestMapping(value = "/handle-product",method = RequestMethod.POST)
-	public RedirectView handleProduct(@ModelAttribute Product product) {
+	public RedirectView handleProduct(@ModelAttribute Product product,HttpServletRequest request) {
 		System.out.println(product);
+		productDao.createProduct(product);
 		RedirectView redirectView = new RedirectView();
+		redirectView.setUrl(request.getContextPath()+"/");
 		return redirectView;
+	}
+	
+	//delete handler
+	@RequestMapping("/delete/{productId}")
+	public RedirectView deleteProduct(@PathVariable("productId") int productId,HttpServletRequest request) {
+		this.productDao.deleteProduct(productId);
+		System.out.println(productId);
+		RedirectView redirectView = new RedirectView();
+		redirectView.setUrl(request.getContextPath()+"/");
+		return redirectView;
+	}
+	
+	//update handler
+	@RequestMapping("/update/{productId}")
+	public String updateForm(@PathVariable("productId") int pid,Model m) {
+		
+		Product product = this.productDao.getProduct(pid);
+		m.addAttribute("product", product);
+		return "update_form";
 	}
 }
--- productcrud/src/main/webapp/WEB-INF/views/update_form.jsp
+++ productcrud/src/main/webapp/WEB-INF/views/update_form.jsp
@@ -8,36 +8,37 @@
 </head>
 <body>
 	
-	<div class="container mt-3">
+	<div class="conatainer mt-3">
 		<div class="row">
 			<div class="col-md-6 offset-md-3">
-				<h1 class="text-center mb-3"> Product Details </h1>
+				<h1 class="text-center mb-3">Change Product Details </h1>
 				
-				<form action="handle-product" method="post">
+				<form action="${pageContext.request.contextPath}/handle-product" method="post">
+				<input type="hidden" name="id" value="${product.id }"/>
 					<div class="form-group mt-3">
 						<label for="name">Product Name</label>
-						<input type="text" 
-								class="form-control" 
-								id="name"
-								name="name" 
-								placeholder="Enter product name">
+						<input type="text" class="form-control" id="name"
+								name="name" placeholder="Enter product name"
+								value="${product.name }">
 					</div>
 					<div class="form-group mt-3">
 						<label for="name">Product Description</label>
 						<textarea rows="5" class="form-control"
 						     	name="description" 
 						     	id="description"
-						     	placeholder="Enter product description"></textarea>
+						     	placeholder="Enter product description">${product.description }
+						     	</textarea>
 					</div>
 					<div class="form-group mt-3">
 						<label for="price">Product Price</label>
 						<input type="text" class="form-control" id="price"
-								name="price" placeholder="Enter product price">
+								name="price" placeholder="Enter product price"
+								value="${product.price }">
 					</div>
 					<div class="container text-center mt-3">
 						<a href="${pageContext.request.contextPath }/"
 						   class="btn btn-outline-danger">Back</a>
-						<button type="submit" class="btn btn-primary">Add</button>
+						<button type="submit" class="btn btn-warning">Update</button>
 					</div>
 				</form>
 			</div>
